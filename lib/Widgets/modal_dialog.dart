7c3891582d5a3c1dd8f014d471714f03c6f346eb
aerodeval <67939367+aerodeval@users.noreply.github.com>
2022-10-09 22:14:17 +0530

    initial UI

---
+++ lib/Widgets/modal_dialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void imagePickerModal(BuildContext context,
+    { VoidCallback? onGalleryTap}) {
+  showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(20),
+          height: 150,
+          child: Column(
+            children: [
+        
+              GestureDetector(
+                onTap: onGalleryTap,
+                child: Card(
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(15),
+                    decoration: const BoxDecoration(color: Colors.grey),
+                    child: const Text("Gallery",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      });
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:ocr/Screen/recognization_page.dart';
+import 'package:ocr/Utils/image_cropper_page.dart';
+import 'package:ocr/Utils/image_picker_class.dart';
+import 'package:ocr/Widgets/modal_dialog.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+     
+      ),
+      home: const MyHomePage(title: 'CyForge- forensics report generator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+backgroundColor:Colors.blueAccent
+ ,     body: Center(
+    
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+  Image.asset('assets/images/cyforge.png'),Row(
+    mainAxisAlignment:MainAxisAlignment.center ,
+    
+    
+    children: [ElevatedButton( child: Text("Generate Report", style: TextStyle(fontSize: 20),) ,onPressed: () {
+          imagePickerModal(context,  onGalleryTap: () {
+            log("Gallery");
+            pickImage(source: ImageSource.gallery).then((value) {
+              if (value != '') {
+                imageCropperView(value, context).then((value) {
+                  if (value != '') {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (_) => RecognizePage(
+                          path: value,
+                        ),
+                      ),
+                    );
+                  }
+                });
+              }
+            });
+          });
+        },) ,
+        SizedBox(width:10),
+ElevatedButton( child: Text("View Reports", style: TextStyle(fontSize: 20),) ,onPressed: (() => {log("View Reports")}))
+
+      ],)      
+
+          ]
+        ),
+      ),
+
+    );
+  }
+}
